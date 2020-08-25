2252bc041e348e0436d30de17ce11c9b04a5c8a5
berwyn <berwyn.codeweaver@gmail.com>
2020-08-25 02:26:29 -0400

    feat(renderer): Start renderer

---
+++ ferrodoom/src/main.rs
@@ -0,0 +1,10 @@
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+
+fn main() {
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+}
